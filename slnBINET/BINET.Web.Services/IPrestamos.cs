b93042f8ef7577c9f8ff05007bab36de19808059
Roger Garay <deeroger@hotmail.com>
2016-01-30 01:10:03 -0500

    To add Prestamos Services

---
+++ slnBINET/BINET.Web.Services/IPrestamos.cs
@@ -0,0 +1,18 @@
+﻿using BINET.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BINET.Web.Services
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IPrestamos" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IPrestamos
+    {
+        [OperationContract]
+        Cliente RegistraPrestamo(string uid, int nCuota,  int nCuenta);
+    }
+}
---
+++ slnBINET/BINET.Web.Services/Prestamos.svc.cs
@@ -0,0 +1,20 @@
+﻿using BINET.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BINET.Web.Services
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Prestamos" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Prestamos.svc o Prestamos.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Prestamos : IPrestamos
+    {
+      public Cliente RegistraPrestamo(string uid, int nCuota,  int nCuenta)
+          {
+              return null;
+          }
+      }
+}
