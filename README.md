f1a3379aa232d3455534ab8bfacd7a05a0f2372c
yelmorp1 <yelmorp@gmail>
2016-02-13 01:36:38 -0500

    invocacion de servicio rest de cliente desde interfast de usuariop

--- README.md
+++
@@ -1,6 +0,0 @@
-# BINET
-Disposición Efectivo Por Campaña En Banca Por Internet
-
-Janet Linares
-Hugo Teccsi
-Hugo treccsesy
--- slnBINET/BINET.Web.UI.WF/ActualizarDatos.aspx.cs
+++ slnBINET/BINET.Web.UI.WF/ActualizarDatos.aspx.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using BINET.Entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +16,19 @@ namespace BINET.Web.UI.WF
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                Cliente clienteObtenido = null;
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:17982/ClientesService.svc/Clientes/1");
+                req.Method = "GET";
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                StreamReader reader = new StreamReader(res.GetResponseStream());
+                string clienteJSON = reader.ReadToEnd();
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                clienteObtenido = js.Deserialize<Cliente>(clienteJSON);
 
+
+            }
         }
 
         protected void btnCancelar_Click(object sender, EventArgs e)
@@ -21,6 +38,21 @@ namespace BINET.Web.UI.WF
 
         protected void btnAceptar_Click(object sender, EventArgs e)
         {
+            string postdata = "{\"IdCli\":\"1\",\"MailCli01\":\"alexander@hotmail.com\",\"TelCli01\":\"999999999\"}";
+            byte[] data = Encoding.UTF8.GetBytes(postdata);
+            HttpWebRequest req = (HttpWebRequest)WebRequest
+                .Create("http://localhost:17982/ClientesService.svc/Clientes");
+            req.Method = "PUT";
+            req.ContentLength = data.Length;
+            req.ContentType = "application/json";
+            var reqStream = req.GetRequestStream();
+            reqStream.Write(data, 0, data.Length);
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string clienteJson = reader.ReadToEnd();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Cliente clienteActualizado = js.Deserialize<Cliente>(clienteJson);
+
             Response.Redirect("Main.aspx");
         }
 
